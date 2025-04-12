40c362b690801d24d350a1f8277e5e5bea602f94
OlegPlatonov <o.a.platonov@gmail.com>
2025-04-13 00:12:55 +0300

    add pokec-regions dataset

--- dataset.py
+++ dataset.py
@@ -26,18 +26,19 @@ class Dataset:
                                     'hm-categories-old', 'web-fraud-old', 'city-roads-M-old', 'city-roads-L-old',
                                     'avazu-devices-old', 'hm-prices-old', 'web-traffic-old']
     tabgraphs_datasets_names = ['hm-categories', 'hm-prices', 'games-categories', 'games-ctr', 'avazu-devices',
-                                'city-reviews', 'city-roads-M', 'city-roads-L', 'twitch-churn', 'twitch-views']
+                                'city-reviews', 'city-roads-M', 'city-roads-L', 'twitch-churn', 'twitch-views',
+                                'pokec-regions']
     pyg_datasets_names = ['roman-empire', 'amazon-ratings', 'minesweeper', 'tolokers', 'questions', 'cora', 'citeseer',
                           'pubmed', 'coauthor-cs', 'coauthor-physics', 'amazon-computers', 'amazon-photo',
                           'lastfm-asia', 'facebook']
     ogb_datasets_names = ['ogbn-arxiv', 'ogbn-products']
 
     # Datasets by task.
-    multiclass_classification_datasets_names = ['hm-categories', 'games-categories', 'browser-games-old',
-                                                'hm-categories-old', 'roman-empire', 'amazon-ratings', 'cora',
-                                                'citeseer', 'pubmed', 'coauthor-cs', 'coauthor-physics',
-                                                'amazon-computers', 'amazon-photo', 'lastfm-asia', 'facebook',
-                                                'ogbn-arxiv', 'ogbn-products']
+    multiclass_classification_datasets_names = ['hm-categories', 'games-categories', 'pokec-regions',
+                                                'browser-games-old', 'hm-categories-old', 'roman-empire',
+                                                'amazon-ratings', 'cora', 'citeseer', 'pubmed', 'coauthor-cs',
+                                                'coauthor-physics', 'amazon-computers', 'amazon-photo', 'lastfm-asia',
+                                                'facebook', 'ogbn-arxiv', 'ogbn-products']
     binary_classification_datasets_names = ['city-reviews', 'twitch-churn', 'tolokers-tab-old', 'questions-tab-old',
                                             'city-reviews-old', 'web-fraud-old', 'minesweeper', 'tolokers', 'questions']
     regression_datasets_names = ['hm-prices', 'games-ctr', 'avazu-devices', 'city-roads-M', 'city-roads-L',
